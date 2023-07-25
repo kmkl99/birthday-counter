40ecd26f951379ae081d7366c3a94c32eb667836
kmkl99 <140562300+kmkl99@users.noreply.github.com>
2023-07-26 04:51:53 +0630

    index.js

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const canvasC = document.getElementById('c');
 
 const config = {
   birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  name: 'KMKL99'
 };
 
 function hideEverything() {
@@ -48,7 +48,7 @@ x = setInterval(function() {
     hw = w / 2, // half-width
     hh = h / 2,
     opts = {
-      strings: ['HAPPY', 'BIRTHDAY!', config.name],
+      strings: ['Lee', 'Pell!', config.name],
       charSize: 30,
       charSpacing: 35,
       lineHeight: 40,
